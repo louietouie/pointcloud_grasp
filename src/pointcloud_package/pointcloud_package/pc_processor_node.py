95ad1b4422404156eaaf1e4873404be8d28bc967
louis <lgparizeau@gmail.com>
2025-01-26 03:43:12 -0500

    cleaned up pointcloud subscriber and created MarkerArray topic publisher for future RViz arrow normals for pointcloud

--- src/pointcloud_package/pointcloud_package/pc_processor_node.py
+++ src/pointcloud_package/pointcloud_package/pc_processor_node.py
@@ -5,14 +5,14 @@ from sensor_msgs.msg import PointCloud2
 import numpy as np
 
 TYPE_MAP = {
-    1: np.int8,
-    2: np.uint8,
-    3: np.int16,
-    4: np.uint16,
-    5: np.int32,
-    6: np.uint32,
-    7: np.float32,
-    8: np.float64
+    1: 'i1', # np.int8
+    2: 'u1', # np.uint8
+    3: 'i2', # np.int16
+    4: 'u2', # np.uint16
+    5: 'i4', # np.int32
+    6: 'u4', # np.uint32
+    7: 'f4', # np.float32
+    8: 'f8', # np.float64
 }
 
 class MinimalSubscriber(Node):
@@ -27,37 +27,27 @@ class MinimalSubscriber(Node):
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        print(msg.fields)
-        print(msg.point_step)
-        pc2 = pcToNumpy(msg)
-        self.get_logger().info('I heard: "%s"' % msg.data[0:20])
-        self.get_logger().info('I heard: "%s"' % msg.fields)
+        pointcloud = pc2ToNumpy(msg)
+        self.get_logger().info('I heard: "%s"' % pointcloud[0])
 
-
-def pcToNumpy(pointcloud2):
-    structured_dtype = pc2FieldsToStructuredDtype(pointcloud2.fields)
-    # numpy_pc = np.array(pointcloud2.data, np.uint8, copy=False).view(structured_dtype)
+def pc2ToNumpy(pointcloud2):
+    structured_dtype = pc2ToStructuredDtype(pointcloud2)
+    return np.array(pointcloud2.data, np.uint8, copy=False).view(structured_dtype)
     # numpy_pc = np.frombuffer(bytes(my_bytes), structured_dtype)
-    # print(structured_dtype)
+
+def pc2ToStructuredDtype(pointcloud2):
+    fields = pointcloud2.fields
+    big_endian = ">" if pointcloud2.is_bigendian else "<"
+    return np.dtype({
+        'names': [f.name for f in fields],
+        'formats': [big_endian + TYPE_MAP.get(f.datatype) for f in fields],
+        'offsets': [f.offset for f in fields],
+        'itemsize':  pointcloud2.point_step
+    })
+
+def pcToNormals(pointcloud):
+
     return 0
-
-
-def pc2FieldsToStructuredDtype(fields):
-    numpy_dtypes = []
-
-    # type2 = np.dtype({
-    #     'names':
-    #     'formats':
-    #     'offsets':
-    #     'itemsize':
-    # })
-
-    print(fields)
-    for field in fields:
-        dtype = (field.name, TYPE_MAP.get(field.datatype))
-        numpy_dtypes.append(dtype)
-    print(numpy_dtypes)
-    return np.dtype(numpy_dtypes)
 
 def main(args=None):
     rclpy.init(args=args)
@@ -72,15 +62,5 @@ def main(args=None):
     minimal_subscriber.destroy_node()
     rclpy.shutdown()
 
-
 if __name__ == '__main__':
     main()
-    # my_bytes = [179, 60, 10, 190, 238, 24, 36, 190, 115, 104, 209, 62, 0, 0, 0, 0, 0, 0, 10, 0]
-    # dtypes = [('x', '<f4'), ('y', '<f4'), ('z', '<f4'), ('i', '<f4'), ('rgb', '<f4')]
-    # # first = np.array(my_bytes)
-    # # print(np.array(my_bytes, dtypes))
-    # # print(first.view(dtypes))
-    # print(np.frombuffer(bytes(my_bytes), dtypes))
-    # print(np.fromiter(my_bytes, dtypes))
-    # print(np.asarray(my_bytes, dtypes, 'K'))
-    # print(np.array(my_bytes, dtype=np.uint8,copy=False).view(dtypes))
