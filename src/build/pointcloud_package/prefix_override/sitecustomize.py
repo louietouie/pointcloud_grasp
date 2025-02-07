717f7344ed477ef3051e0ce0582a8e3003a8423b
louis <lgparizeau@gmail.com>
2025-02-07 16:53:01 -0500

    removed colcon folders from a build in wrong directory

--- src/build/pointcloud_package/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/louis/Documents/ros2_projects/pointcloud_grasp/src/install/pointcloud_package'
