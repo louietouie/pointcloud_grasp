da71ab7ed1fa1c5e651026c338e86e701a4f1132
louis <lgparizeau@gmail.com>
2025-02-17 13:58:08 -0500

    added empty trajectory package and updated readme

---
+++ src/trajectory_package/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'trajectory_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='louis',
+    maintainer_email='lgparizeau@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'trajectory_viewer_node = trajectory_package.trajectory_viewer_node:main'
+        ],
+    },
+)
---
+++ src/trajectory_package/trajectory_package/trajectory_viewer_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from trajectory_package.')
+
+
+if __name__ == '__main__':
+    main()
